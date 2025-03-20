c9747340abdce535d438024a6c3aa654ad01a20d
bhavuk1017 <134728120+bhavuk1017@users.noreply.github.com>
2025-03-20 14:06:57 +0530

    commit

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.post("/submit-test", async (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
